c36b1b7be6ff7dbcb158dd06c360b7ccb6057724
Jacob Yaw <yawjacob@yahoo.com>
2022-05-05 20:50:49 -0500

    commit tree e650e7f2ec91a9af36bbb783ee8a898edcbac7ee

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < Sinatra::Base
+    get '/' do
+        '<h1>Hello everyone! welcome to my <em>Worldsssssssssssssss</em>!</h1>
+        
+        <p>Im learning so much from this program. Its pretty stressful at times but I have faith that things will get better and Ill land a job as a software developer!</p>
+            <br></br>
+        <p>now observe this unordered list!</p>
+            <br></br>
+            <ul>
+                <li>HI EVERYONE!!</li>
+                <li>HI!!!!</li>
+                <li>OK GOODBYE NOW!!</li>
+                <li>BYE!!!!!</li>
+            </ul>
+
+            <p>ACK!</p>
+        '
+    end
+end
